2dc6816dc9629c4a1586b1b24aa2e99a11ba06c5
freddy-mendez <freddy.mendez@misena.edu.co>
2021-11-12 14:05:06 -0500

    procesos firebase
    crud basico

--- app/src/main/java/co/edu/unab/mercapp/FireBaseActivity.java
+++ app/src/main/java/co/edu/unab/mercapp/FireBaseActivity.java
@@ -5,21 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import co.edu.unab.mercapp.entity.Producto;
+
 public class FireBaseActivity extends AppCompatActivity {
     private static final String TAG = "FirebaseActivity";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference productos = db.collection("productos");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire_base);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+    }
+
+    public void listar(View view) {
+        cargarLista();
+    }
+
+    private void cargarLista() {
+        EditText txtResultado = findViewById(R.id.txtResultado);
+        txtResultado.setText("");
         db.collection("productos")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -27,12 +50,131 @@ public class FireBaseActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                //Log.d(TAG, document.getId() + " => " + document.getData());
+                                Producto producto = document.toObject(Producto.class);
+                                //Log.d(TAG, document.getId()+" => "+producto);
+                                txtResultado.append(document.getId()+" => "+producto+"\n");
+
                             }
                         } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
+                            //Log.w(TAG, "Error getting documents.", task.getException());
+                            txtResultado.setText("Error getting documents."+task.getException());
                         }
                     }
                 });
+    }
+
+    public void agregar(View view) {
+        EditText txtData = findViewById(R.id.txtData);
+        String [] valores = txtData.getText().toString().split(";");
+        Producto producto = null;
+        if (valores.length==4) {
+            producto = new Producto(valores[0], valores[1],
+                    Integer.parseInt(valores[2]), Boolean.parseBoolean(valores[3]));
+        } else {
+            producto = new Producto(valores[0], valores[1],
+                    Integer.parseInt(valores[2]));
+        }
+        Map<String, Object> infoProducto = new HashMap<>();
+        infoProducto.put("codigo", valores[0]);
+        infoProducto.put("nombre", valores[1]);
+        infoProducto.put("precio", Integer.parseInt(valores[2]));
+        productos.add(infoProducto).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                cargarLista();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                EditText txtResultado = findViewById(R.id.txtResultado);
+                txtResultado.setText("Error agregando un documento."+e.getMessage());
+            }
+        });
+    }
+
+    public void eliminar(View view) {
+        EditText txtIdDoc = findViewById(R.id.txtIdDoc);
+        DocumentReference producto = productos.document(txtIdDoc.getText().toString());
+
+        producto.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()) {
+                        producto.delete();
+                        Toast.makeText(FireBaseActivity.this, "Producto Eliminado", Toast.LENGTH_SHORT).show();
+                        cargarLista();
+                    } else {
+                        Toast.makeText(FireBaseActivity.this, "ERROR al Eliminar el Producto", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        /*producto.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(FireBaseActivity.this, "Producto Eliminado", Toast.LENGTH_SHORT).show();
+                    cargarLista();
+                } else {
+                    Toast.makeText(FireBaseActivity.this, "ERROR al Eliminar el Producto", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });*/
+    }
+
+    public void update(View view) {
+        EditText txtIdDoc = findViewById(R.id.txtIdDoc);
+        DocumentReference producto = productos.document(txtIdDoc.getText().toString());
+        EditText txtNewData = findViewById(R.id.txtNewData);
+        String [] valores = txtNewData.getText().toString().split(";");
+        Map<String, Object> values = new HashMap<>();
+        for (String item: valores) {
+            String [] atributos = item.split(":");
+            if (atributos[0].equals("precio")) {
+                values.put(atributos[0], Integer.parseInt(atributos[1]));
+            } else if (atributos[0].equals("disponible")) {
+                values.put(atributos[0], Boolean.parseBoolean(atributos[1]));
+            } else {
+                values.put(atributos[0], atributos[1]);
+            }
+        }
+
+        producto.update(values).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                cargarLista();
+            }
+        });
+
+
+    }
+
+    public void setData(View view) {
+        EditText txtIdDoc = findViewById(R.id.txtIdDoc);
+        DocumentReference producto = productos.document(txtIdDoc.getText().toString());
+        EditText txtNewData = findViewById(R.id.txtNewData);
+        String [] valores = txtNewData.getText().toString().split(";");
+        Map<String, Object> values = new HashMap<>();
+        for (String item: valores) {
+            String [] atributos = item.split(":");
+            if (atributos[0].equals("precio")) {
+                values.put(atributos[0], Integer.parseInt(atributos[1]));
+            } else if (atributos[0].equals("disponible")) {
+                values.put(atributos[0], Boolean.parseBoolean(atributos[1]));
+            } else {
+                values.put(atributos[0], atributos[1]);
+            }
+        }
+
+        producto.set(values).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                cargarLista();
+            }
+        });
     }
 }
--- app/src/main/java/co/edu/unab/mercapp/entity/Producto.java
+++ app/src/main/java/co/edu/unab/mercapp/entity/Producto.java
@@ -1,4 +1,68 @@
 package co.edu.unab.mercapp.entity;
 
 public class Producto {
+    private String codigo;
+    private String nombre;
+    private int precio;
+    private boolean disponible;
+
+    public Producto() {
+
+    }
+
+    public Producto(String codigo, String nombre, int precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.disponible = true;
+    }
+
+    public Producto(String codigo, String nombre, int precio, boolean disponible) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.disponible = disponible;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", disponible=" + disponible +
+                '}';
+    }
 }
